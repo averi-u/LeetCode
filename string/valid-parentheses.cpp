73e9b371ef0f56ee836490c08b4d63148479adb0
Averi Yu / Zhizhen Yu <qq2388115736@gmail.com>
2026-03-19 14:27:22 -0700

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ string/valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+       int getTypeLeft(char c) {
+      if (c == '(') return 1;
+      else if (c == '[') return 2;
+      else if (c == '{') return 3;
+      else return -1;
+    }
+    int getTypeRight(char c) {
+      if (c == ')') return 1;
+      else if (c == ']') return 2;
+      else if (c == '}') return 3;
+      else return -1;      
+    }
+
+    bool isValid(string s) {
+        stack<int> stk;
+        for (int i = 0; i < s.size(); i++) {
+          int LEFT_TYPE = getTypeLeft(s[i]);
+
+          // right bracket
+          if (LEFT_TYPE == -1) {
+            if (stk.empty()) return false;
+            int RIGHT_TYPE = getTypeRight(s[i]);
+            if (RIGHT_TYPE == -1) return false;
+            if (stk.top() != RIGHT_TYPE) return false;
+            else stk.pop();
+          // left bracket
+          } else {
+            stk.push(LEFT_TYPE);
+          }
+        }
+        return stk.empty();
+};
