b2ac0bc7be99b097f6b3c4e0a7edbd2515674d33
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-05-16 14:50:33 -0700

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.java
+++ array/3sum.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-    List<List<Integer>> outputs = null;
-    for(int i = 0; i < nums.length-2; i+=1) {
-        for (int j = i +1; j <nums.length - 1; j+=1) {
-          for (int k = i +1; j <nums.length -2; k+=1) {
-            Boolean isvalid = false;
-            if (nums[i]!=nums[j] & nums[i]!= nums[k] & nums[j]!=nums[k]) {
-              if (nums[i] + nums[j] + nums[k] == 0) {
-                isvalid= true;
-                if (isvalid) {
-                List<Integer> validseq = null;
-				        validseq.add(i);
-                validseq.add(j);
-                validseq.add(k);
-                outputs.add(validseq);
+        List<List<Integer>> outputs = null;
+        for(int i = 0; i < nums.length-2; i+=1) {
+            for (int j = i +1; j <nums.length - 1; j+=1) {
+            for (int k = i +1; j <nums.length -2; k+=1) {
+                Boolean isvalid = false;
+                if (nums[i]!=nums[j] & nums[i]!= nums[k] & nums[j]!=nums[k]) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    isvalid= true;
+                    if (isvalid) {
+                    List<Integer> validseq = null;
+                            validseq.add(i);
+                    validseq.add(j);
+                    validseq.add(k);
+                    outputs.add(validseq);
+                    }
+                    
                 }
-                
-              }
+                }
             }
-          }
+            }
         }
-    }
-    return outputs;
+        return outputs;
     }
 }
