3d67d8bb4c412ce444d485cede55f96114ce3621
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-05-16 14:35:10 -0700

    LC-3sum [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/3sum.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n): 
+            if i > 0 and nums[i] == nums[i - 1]: 
+                continue 
+            target = -nums[i]
+            l, r = i + 1, n-1
+
+            while l < r : 
+                s = nums[l] + nums[r]
+                if s == target: 
+                    res.append([nums[i], nums[l], nums[r]])
+
+                    l_val, r_val = nums[l], num[r]
+
+                    while l < r and nums[l] == l_val: 
+                        l += 1 
+                    while l < r and nums[r] == r_val: 
+                        r -= 1 
+
+                elif s < target: 
+                    l += 1 
+                else: 
+                    r -= 1 
+
+            return res 
