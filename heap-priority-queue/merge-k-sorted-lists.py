889f15fbe778795dc3a8b21327a17522d778525c
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-05-18 15:37:23 -0700

    LC-merge-k-sorted-lists [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/merge-k-sorted-lists.py
+++ heap-priority-queue/merge-k-sorted-lists.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from queue import PriorityQueue
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         newList = ListNode()
