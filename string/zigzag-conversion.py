3e0837e482a5b5936f3ef867653e626b0506078d
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-06-26 10:41:17 -0700

    LC-zigzag-conversion [Runtime: 52 ms; Memory: 19.2 MB]

--- string/zigzag-conversion.py
+++ string/zigzag-conversion.py
@@ -1,6 +1,6 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-                if numRows == 1 or numRows >= len(s):
+        if numRows == 1 or numRows >= len(s):
             return s
 
         rows = [""] * numRows
