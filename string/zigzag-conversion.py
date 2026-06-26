69898b457ba0f6525bf87ec388d163b45a70e8d0
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-06-26 10:41:04 -0700

    LC-zigzag-conversion [Runtime: N/A; Memory: N/A]

---
+++ string/zigzag-conversion.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+                if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        row = 0
+        direction = -1
+
+        for c in s:
+            rows[row] += c
+
+            if row == 0 or row == numRows - 1:
+                direction *= -1
+
+            row += direction
+
+        return "".join(rows)
