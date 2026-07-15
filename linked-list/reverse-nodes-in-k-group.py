e6a08799df30afe8db1088177f03353c84969c5f
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-07-14 19:05:52 -0700

    LC-reverse-nodes-in-k-group [Runtime: N/A; Memory: N/A]

--- linked-list/reverse-nodes-in-k-group.py
+++ linked-list/reverse-nodes-in-k-group.py
@@ -1,38 +1,37 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> 
-        Optional[ListNode], k: int) -> Optional[ListNode]:
-            if head is None or k == 1:
-                return head
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None or k == 1:
+            return head
 
-            dummy = ListNode(0, head)
-            group_prev = dummy
+        dummy = ListNode(0, head)
+        group_prev = dummy
 
-            while True:
-                # 1) Find the end of the next k-group
-                kth = group_prev
-                for _ in range(k):
-                    kth = kth.next
-                    if kth is None:
-                        return dummy.next  # less than k nodes left, done
+        while True:
+            # 1) Find the end of the next k-group
+            kth = group_prev
+            for _ in range(k):
+                kth = kth.next
+                if kth is None:
+                    return dummy.next  # less than k nodes left, done
 
-                group_start = group_prev.next
-                group_next = kth.next  # node after the group
+            group_start = group_prev.next
+            group_next = kth.next  # node after the group
 
-                # 2) Reverse the group [group_start .. kth]
-                prev = group_next
-                curr = group_start
-                while curr is not group_next:
-                    nxt = curr.next
-                    curr.next = prev
-                    prev = curr
-                    curr = nxt
+            # 2) Reverse the group [group_start .. kth]
+            prev = group_next
+            curr = group_start
+            while curr is not group_next:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
 
-                # 3) Reconnect: group_prev -> kth (new head), group_start (new tail) -> group_next
-                group_prev.next = kth
-                group_prev = group_start
+            # 3) Reconnect: group_prev -> kth (new head), group_start (new tail) -> group_next
+            group_prev.next = kth
+            group_prev = group_start
