98bef6a502cdb8c66ea52b43e91d72d595e23dc2
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-07-28 12:05:21 -0700

    LC-integer-to-roman [Runtime: N/A; Memory: N/A]

--- math/integer-to-roman.py
+++ math/integer-to-roman.py
@@ -1,7 +1,6 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        class Solution:
-    def intToRoman(self, num: int) -> str:
+        
         values = [1000, 900, 500, 400,
                   100, 90, 50, 40,
                   10, 9, 5, 4, 1]
