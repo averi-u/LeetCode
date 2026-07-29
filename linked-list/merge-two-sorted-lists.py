f285a4c578415ffc45bceb9ef3585d63323dd852
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-07-29 15:16:39 -0700

    LC-merge-two-sorted-lists [Runtime: N/A; Memory: N/A]

--- linked-list/merge-two-sorted-lists.py
+++ linked-list/merge-two-sorted-lists.py
@@ -9,12 +9,12 @@ class Solution:
         tail = dummy
 
         while list1 and list2:
-        if list1.val <= list2.val:
-            tail.next = list1
-            list1 = list1.next
-        else:
-            tail.next = list2
-            list2 = list2.next
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
         tail = tail.next
 
         tail.next = list1 if list1 else list2 
