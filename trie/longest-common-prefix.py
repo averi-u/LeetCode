8b5a879e48e6300c5bcefc9316e4840c2f825a75
Averie Zhizhen Yu <qq2388115736@gmail.com>
2026-07-29 09:43:16 -0700

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- trie/longest-common-prefix.py
+++ trie/longest-common-prefix.py
@@ -3,12 +3,15 @@ class Solution:
         res=strs[0]
 
         for item in strs:
+            if len(item)==0:
+                return ""
+                
             for x in range(len(item)):
                 if item[x]!=res[x]:
                     break
             if x==0:
                 return ""
-                
+
             if x<=len(item):
                 res=item[0:x]
 
